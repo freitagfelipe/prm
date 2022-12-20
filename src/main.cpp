5fe5bf0d0e73dda4ac48b55a790d5caf77ed0d8a
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-12-20 11:29:03 -0300

    Update main adding de new add and remove commands

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
 #include <CLI/CLI.hpp>
+#include <commands.hpp>
 #include <iostream>
 
 int main(int argc, char **argv) {
     CLI::App app {"PRM - A simple and ease-to-use personal repository manager"};
+    Commands commands;
+
+    commands.add(app);
+    commands.remove(app);
+
+    app.preparse_callback([](size_t arg_count) {
+        if (arg_count == 0) {
+            throw CLI::CallForHelp();
+        }
+    });
 
     CLI11_PARSE(app, argc, argv);
-
-    std::cout << app.help() << std::endl;
 
     return 0;
 }
