318a3902cbdf7521c310d0d315f5b737a5169cf7
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-12-23 03:37:55 -0300

    Define store header and implement the add repository method

---
+++ include/store.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+namespace store {
+    void add_repository(std::string &name, std::string &clone_link, std::string &category);
+}
---
+++ src/store.cpp
@@ -0,0 +1,51 @@
+#include <store.hpp>
+#include <nlohmann/json.hpp>
+#include <colors.hpp>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+void store::add_repository(std::string &name, std::string &repository_link, std::string &category) {
+    std::fstream f;
+
+    f.open(".prm.json", std::ios::in | std::ios::out);
+
+    if (!f.is_open()) {
+        std::cerr << colors::red << "Can't open the file" << std::endl;
+
+        std::exit(1);
+    }
+
+    nlohmann::json j {nlohmann::json::parse(f)};
+    nlohmann::json new_value = {
+        {"link", repository_link},
+        {"name", name}
+    };
+
+    j = j.at(0);
+
+    for (auto &[curr_category, curr_value] : j.items()) { 
+        if (curr_value.is_null()) {
+            continue;
+        }
+
+        for (auto &[key, value] : curr_value.items()) {
+            if (value["name"] == name && value["link"] == repository_link) {
+                std::cerr << colors::red << "You already have this repository!" << std::endl;
+
+                std::exit(2);
+            }
+        }
+    }
+
+    if (j[category].is_null()) {
+        j["created"] = std::vector<nlohmann::json>({new_value});
+    } else {
+        j["created"].push_back(new_value);
+    }
+
+    f.seekp(0);
+    f.clear();
+
+    f << std::setw(4) << j;
+}
