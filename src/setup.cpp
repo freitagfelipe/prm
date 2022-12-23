11db7fbe2ca957fc797151c63f9adb4e362bd626
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2022-12-23 03:37:07 -0300

    Define setup header and implement the init method

---
+++ src/setup.cpp
@@ -0,0 +1,36 @@
+#include <setup.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <nlohmann/json.hpp>
+#include <colors.hpp>
+
+void setup::init() {
+    std::fstream f;
+
+    f.open(".prm.json", std::ios::in);
+
+    if (f.is_open()) {
+        return;
+    }
+
+    nlohmann::json j = {
+        {"created", {}},
+        {"idle", {}},
+        {"working", {}},
+        {"finished", {}}
+    };
+
+    f.open(".prm.json", std::ios::out);
+
+    if (f.is_open()) {
+        f << std::setw(4) << j;
+
+        return;
+    }
+
+    std::cerr << colors::red << "Can't open the file" << std::endl;
+
+    std::exit(1);
+}
