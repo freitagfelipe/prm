0a28236159619a1e102a011dee9c68c8528854d5
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-01-08 14:45:10 -0300

    Add linux/macOS installer

---
+++ installer/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Installing PRM..."
+
+if ! [ -x "$(command -v cmake)" ]; then
+    echo "You need CMake to build PRM"
+
+    exit 1
+elif ! [ -x "$(command -v make)" ]; then
+    echo "You need make to build PRM"
+
+    exit 1
+fi
+
+cd ..
+mkdir build
+cd build
+cmake ..
+cmake --build . --config Release
+
+if [ ! -d $HOME/.local/bin ]; then
+    echo "Creating the following folder to store the executable: $HOME/.local/bin"
+
+    mkdir -p $HOME/.local/bin
+fi
+
+echo "Moving the PRM executable to the $HOME/.local/bin"
+
+mv prm $HOME/.local/bin
+
+echo "Finished the installation"
